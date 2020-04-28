cbb1c1a3d4a75380cdf4aeb67c4c793dd525d50c
ishpartko <ishpartko@gmail.com>
2020-04-29 01:06:19 +0300

    update linting

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/calendarOfAgeingMotivation/" : "/"
+  publicPath: process.env.NODE_ENV === 'production' ? '/calendarOfAgeingMotivation/' : '/'
 }
